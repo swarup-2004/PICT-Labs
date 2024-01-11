aa8151add61ec65e1aeaf26bbde643fc026ec4a8
Swarup Vijay Pokharkar <110724501+swarup-2004@users.noreply.github.com>
2024-01-11 09:48:58 +0530

    Add files via upload

--- General_Tree.cpp
+++ General_Tree.cpp
@@ -3,51 +3,58 @@
 #include <vector>
 using namespace std;
 
-class TreeNode {
+class TreeNode
+{
 public:
     string data;
     vector<TreeNode *> childNodes;
 
     // Constructor to initialize data
-    TreeNode(const string& d) : data(d) {}
+    TreeNode(const string &d) : data(d) {}
 };
 
-class GeneralTree {
+class GeneralTree
+{
     TreeNode *root;
 
 public:
     // Constructor to initialize root using makeTree
-    GeneralTree() : root(nullptr) {
+    GeneralTree()
+    {
+        string data;
+        cout << "Making root Level 0" << endl;
+        cout << "Enter data :";
+        cin >> data;
+        root = new TreeNode(data);
+
         root = makeTree(root);
     }
 
     // Function to create a tree recursively
-    TreeNode *makeTree(TreeNode *root, int level = 0) {
+    TreeNode *makeTree(TreeNode *root, int level = 0)
+    {
         string data;
-        if (root == nullptr) {
-            cout << "Making root Level 0" << endl;
-            cout << "Enter data :";
-            cin >> data;
-            root = new TreeNode(data);
-        }
 
         int ch;
-        do {
+        do
+        {
             cout << "Level :" << level + 1 << endl;
             cout << "1. Add more nodes\n2. Stop" << endl;
             cout << "Enter your choice :";
             cin >> ch;
-            if (ch == 1) {
+            if (ch == 1)
+            {
                 int nodeType;
                 cout << "Making node at Level " << level + 1 << endl;
                 cout << "1. Internal Node\n2. Leaf Node" << endl;
                 cout << "Enter your choice :";
                 cin >> nodeType;
-                cout << "Enter data :" << endl;
+                cout << "Enter data :";
                 cin >> data;
                 TreeNode *newNode = new TreeNode(data);
                 root->childNodes.push_back(newNode);
-                if (nodeType != 2) {
+                if (nodeType == 1)
+                {
                     // Recursively call makeTree for internal nodes
                     makeTree(newNode, level + 1);
                 }
@@ -58,22 +65,29 @@ public:
     }
 
     // Function to display the tree
-    void displayTree(TreeNode *node, int level = 0) {
-        if (!node) {
-        	cout << " -1 ";
-            return;
-        }
+    void displayTree(TreeNode *node)
+    {
 
         cout << node->data << " ";
 
-        for (const auto &child : node->childNodes) {
-            displayTree(child, level + 1);
+        if (node->childNodes.empty())
+        {
+            cout << "-1"
+                 << " ";
+        }
+
+        for (const auto &child : node->childNodes)
+        {
+
+            displayTree(child);
         }
     }
 
     // Wrapper function to start displaying from the root
-    void display() {
-        if (root == nullptr) {
+    void display()
+    {
+        if (root == nullptr)
+        {
             cout << "Tree is empty." << endl;
             return;
         }
@@ -83,7 +97,8 @@ public:
     }
 };
 
-int main() {
+int main()
+{
     GeneralTree t1;
     t1.display();
     return 0;
