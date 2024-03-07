642c1ad43d5e3518e599313141c64948610bbdf9
Swarup Vijay Pokharkar <110724501+swarup-2004@users.noreply.github.com>
2024-03-07 10:49:12 +0530

    Add files via upload

---
+++ DSAL/Assignment_No_6_DSAL.cpp
@@ -0,0 +1,288 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <set>
+#include <stack>
+using namespace std;
+
+// Forword Declaration
+class Graph;
+
+class GraphNode
+{
+    string city;     // Name of the city
+    GraphNode *next; // Nodes connected to the current node
+    GraphNode *down; // Next Node in the list
+
+public:
+    // Declaring Friend Class
+    friend class Graph;
+
+    // Default constructor
+    GraphNode()
+    {
+        next = down = nullptr;
+    }
+    // Parameterized constructor
+    GraphNode(string city)
+    {
+        this->city = city;
+        next = down = nullptr;
+    }
+};
+
+class Graph
+{
+
+    GraphNode *head; // Pointer to the array storing adjacency list
+    int size;        // Size of the graph
+
+public:
+    // Constructor
+    Graph(int size)
+    {
+        this->size = size;
+        head = new GraphNode("City");
+        head = createGraph();
+    }
+
+    // Creating a Graph
+    GraphNode *createGraph()
+    {
+        GraphNode *temp = head;
+        // Traversing through each vertex
+        for (int i = 0; i < size; i++)
+        {
+            string city;
+            int degree;
+            cout << "Enter the city :";
+            cin >> city;
+            cout << "Enter the number of connected cities :";
+            cin >> degree;
+            GraphNode *newNode = new GraphNode(city);
+            GraphNode *temp2 = newNode;
+            for (int i = 0; i < degree; i++)
+            {
+                string city;
+                cout << "Enter the city connected to the " << newNode->city << " : ";
+                cin >> city;
+                GraphNode *cityNode = new GraphNode(city);
+                temp2->next = cityNode;
+                temp2 = temp2->next;
+            }
+
+            temp->down = newNode;
+            temp = temp->down;
+        }
+        return head;
+    }
+
+    // Displayig Adjacency List
+    void display()
+    {
+        if (head == nullptr)
+        {
+            cout << "Empty List" << endl;
+            return;
+        }
+        GraphNode *temp = head->down;
+        while (temp != nullptr)
+        {
+            cout << temp->city << " : ";
+            GraphNode *temp2 = temp->next;
+            while (temp2 != nullptr)
+            {
+                cout << temp2->city << " ";
+                temp2 = temp2->next;
+            }
+            cout << endl;
+            temp = temp->down;
+        }
+    }
+
+    // Finding out degree of the Nodes
+    void degree()
+    {
+        if (head == nullptr)
+        {
+            cout << "Empty List" << endl;
+            return;
+        }
+        GraphNode *temp = head->down;
+        while (temp != nullptr)
+        {
+
+            int count = 0;
+            GraphNode *temp2 = temp->next;
+            while (temp2 != nullptr)
+            {
+                count++;
+                temp2 = temp2->next;
+            }
+            cout << temp->city << " : " << count << endl;
+            temp = temp->down;
+        }
+    }
+
+    // Function for the BFS
+    void BFS()
+    {
+        set<string> visited;
+        GraphNode *temp = head->down;
+        queue<GraphNode *> q;
+        q.push(temp);
+        while (!q.empty())
+        {
+            GraphNode *popped = q.front();
+            q.pop();
+            auto vis = visited.find(popped->city);
+            if (vis == visited.end())
+            {
+                cout << popped->city << " ";
+                visited.insert(popped->city);
+                GraphNode *temp2 = popped->next;
+                while (temp2 != nullptr)
+                {
+                    auto temp2_vis = visited.find(temp2->city);
+                    if (temp2_vis == visited.end())
+                    {
+                        GraphNode *temp3 = temp->down;
+                        while (temp3 != nullptr)
+                        {
+                            if (temp3->city == temp2->city)
+                            {
+                                q.push(temp3);
+                                break;
+                            }
+                            temp3 = temp3->down;
+                        }
+                    }
+                    temp2 = temp2->next;
+                }
+            }
+        }
+        cout << endl;
+    }
+
+    // Helper function for the BFS
+    void bfsHelp(GraphNode *temp, set<string> &visited)
+    {
+        queue<GraphNode *> q;
+        q.push(temp);
+        while (!q.empty())
+        {
+            GraphNode *popped = q.front();
+            q.pop();
+            auto vis = visited.find(popped->city);
+            if (vis == visited.end())
+            {
+                cout << popped->city << " ";
+                visited.insert(popped->city);
+                GraphNode *temp2 = popped->next;
+                while (temp2 != nullptr)
+                {
+                    auto temp2_vis = visited.find(temp2->city);
+                    if (temp2_vis == visited.end())
+                    {
+                        GraphNode *temp3 = temp->down;
+                        while (temp3 != nullptr)
+                        {
+                            if (temp3->city == temp2->city)
+                            {
+                                q.push(temp3);
+                                break;
+                            }
+                            temp3 = temp3->down;
+                        }
+                    }
+                    temp2 = temp2->next;
+                }
+            }
+        }
+    }
+
+    // Function for the BFS
+    void bfs()
+    {
+        set<string> visited;
+        GraphNode *temp = head->down;
+        while (temp != nullptr)
+        {
+            auto vis = visited.find(temp->city);
+            if (vis == visited.end())
+            {
+                bfsHelp(temp, visited);
+                cout << endl;
+            }
+            temp = temp->down;
+        }
+    }
+
+    // Helper function for DFS
+    void dfsHelp(GraphNode *temp, set<string> &visited)
+    {
+        stack<GraphNode *> stk;
+        stk.push(temp);
+        while (!stk.empty())
+        {
+            GraphNode *current = stk.top();
+            stk.pop();
+            string currcity;
+            if (visited.find(current->city) == visited.end())
+            {
+                currcity = current->city;
+                cout << current->city << " ";
+                visited.insert(current->city);
+            }
+            GraphNode *temp2 = temp->next;
+
+            while (temp2 != nullptr)
+            {
+                string nextcity;
+                auto vis = visited.find(temp2->city);
+                if (vis == visited.end())
+                {
+                    nextcity = temp2->city;
+                    GraphNode *temp3 = temp->down;
+                    while (temp3 != nullptr)
+                    {
+                        if (temp3->city == temp2->city)
+                        {
+                            stk.push(temp3);
+                            break;
+                        }
+                        temp3 = temp3->down;
+                    }
+                }
+                temp2 = temp2->next;
+            }
+        }
+    }
+
+    // Function for the DFS
+    void dfs()
+    {
+        set<string> visited;
+        GraphNode *temp = head->down;
+        while (temp != nullptr)
+        {
+            auto vis = visited.find(temp->city);
+            if (vis == visited.end())
+            {
+                dfsHelp(temp, visited);
+                cout << endl;
+            }
+            temp = temp->down;
+        }
+    }
+};
+
+int main()
+{
+    Graph g(4);
+    g.display();
+    g.degree();
+    g.bfs();
+    g.dfs();
+    return 0;
+}
---
+++ DSAL/Assignment_No_7_DSAL.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+// Forward declaration
+class Graph;
+
+// Class representing an edge in the graph
+class Edge
+{
+    int src;
+    int dest;
+    int wei;
+
+public:
+    friend class Graph;
+
+    // Constructor for Edge
+    Edge(int src, int dest, int wei)
+    {
+        this->src = src;
+        this->dest = dest;
+        this->wei = wei;
+    }
+
+    // Overloading output stream operator for Edge
+    friend ostream &operator<<(ostream &os, const Edge &edge)
+    {
+        os << edge.src << "->" << edge.dest << " (Weight: " << edge.wei << ")";
+        return os;
+    }
+};
+
+// Class representing a pair of office and its distance
+class Pair
+{
+    int office;
+    int distance;
+
+public:
+    friend class Graph;
+    // Constructor for Pair
+    Pair(int office, int distance)
+    {
+        this->office = office;
+        this->distance = distance;
+    }
+
+    // Overloading less than operator for Pair
+    bool operator<(const Pair &p2) const
+    {
+        return distance > p2.distance; // For min priority queue
+    }
+};
+
+// Class representing the graph
+class Graph
+{
+    vector<Edge> *graph;
+    int size;
+
+public:
+    // Constructor for Graph
+    Graph(int size)
+    {
+        this->size = size;
+        graph = new vector<Edge>[size];
+        for (int i = 0; i < size; i++)
+        {
+            vector<Edge> v;
+            graph[i] = v;
+        }
+        create();
+    }
+
+    // Function to create the graph
+    void create()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            int degree;
+            cout << "Enter the number of connected offices for office " << i << " :";
+            cin >> degree;
+            for (int j = 0; j < degree; j++)
+            {
+                int office, distance;
+                cout << "Enter the connected office to office " << i << " :";
+                cin >> office;
+                cout << "Enter the distance between office " << i << " and office " << office << " :";
+                cin >> distance;
+                graph[i].push_back(Edge(i, office, distance));
+            }
+        }
+    }
+
+    // Function to display the graph
+    void display()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            cout << "Edges from office " << i << ": ";
+            for (int j = 0; j < graph[i].size(); j++)
+            {
+                cout << graph[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    // Function to find the minimum spanning tree cost
+    int minimumSpanningTree()
+    {
+        int cost = 0;
+        priority_queue<Pair> pq;
+        bool visited[size] = {false}; // Array to keep track of visited offices
+
+        // Start with office 0
+        pq.push(Pair(0, 0));
+
+        // Loop until priority queue is empty
+        while (!pq.empty())
+        {
+            Pair current = pq.top();
+            pq.pop();
+
+            // If the office is not visited yet
+            if (!visited[current.office])
+            {
+                visited[current.office] = true; // Mark the office as visited
+                cost += current.distance;       // Add distance to the total cost
+
+                // Explore all neighbors of the current office
+                for (int i = 0; i < graph[current.office].size(); i++)
+                {
+                    Edge neighbor = graph[current.office][i];
+                    if (!visited[neighbor.dest])
+                    {
+                        // Push neighbor into the priority queue
+                        // Negative weight is used for minimum priority
+                        pq.push(Pair(neighbor.dest, neighbor.wei));
+                    }
+                }
+            }
+        }
+        return cost;
+    }
+};
+
+int main()
+{
+    int numOffices;
+    cout << "Enter the number of offices: ";
+    cin >> numOffices;
+    Graph g(numOffices);
+    g.display();
+    cout << "Minimum spanning tree cost: " << g.minimumSpanningTree() << endl;
+    return 0;
+}
